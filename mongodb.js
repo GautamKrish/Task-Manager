f39a0a1ccc39be68ab9125c2424ba7e46bf8cddf
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-07-18 19:19:40 +0530

    exploring object ID

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,12 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID} = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const dbName = 'task-manager'
+const id = new ObjectID()
+console.log(`Object ID :- ${id}`)
+console.log(`timestamp :- ${id.getTimestamp()}`)
+console.log(`length :- ${id.id.length}`)
+console.log(`In string :- ${id.toHexString().length}`)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
@@ -25,23 +29,23 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     //         console.log(result.ops)
     //     })
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'Mongodb course',
-            completed: false
-        },
-        {
-            description: 'Read for half an hour',
-            completed: false
-        },
-        {
-            description: 'Watch a podcast',
-            completed: false
-        }], (error, result) => {
-            if (error) {
-                return console.log('Unable to insert into the collection')
-            }
-            console.log('Successfully added tasks to the tasks collection \n', result.ops)
-        })
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Mongodb course',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'Read for half an hour',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'Watch a podcast',
+    //         completed: false
+    //     }], (error, result) => {
+    //         if (error) {
+    //             return console.log('Unable to insert into the collection')
+    //         }
+    //         console.log('Successfully added tasks to the tasks collection \n', result.ops)
+    //     })
 })
 
