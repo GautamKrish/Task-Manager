f83d6a2689b60edb9940f28807b9c8a93a7ea5cc
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-07-18 15:27:10 +0530

    setup task-manager mongo database.

--- mongodb.js
+++ mongodb.js
@@ -4,15 +4,44 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const dbName = 'task-manager'
 
-MongoClient.connect(connectionURL, {useNewUrlParser : true, useUnifiedTopology : true}, (error, client) => {
-    if(error){
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
         return console.log('Unable to connect to mongo db')
     }
 
     console.log('Successfully connected to mongodb')
     const db = client.db(dbName)
-    db.collection('users').insertOne({
-        name : 'Gautam',
-        age  :23
-    })
+    //     db.collection('users').insertMany([{
+    //         name : 'Katta',
+    //         age  :23
+    //     },
+    //     {
+    //         name : 'Chandan',
+    //         age  :23
+    //     }], (error, result) => {
+    //         if(error){
+    //             return console.log(error)
+    //         }
+    //         console.log(result.ops)
+    //     })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Mongodb course',
+            completed: false
+        },
+        {
+            description: 'Read for half an hour',
+            completed: false
+        },
+        {
+            description: 'Watch a podcast',
+            completed: false
+        }], (error, result) => {
+            if (error) {
+                return console.log('Unable to insert into the collection')
+            }
+            console.log('Successfully added tasks to the tasks collection \n', result.ops)
+        })
 })
+
