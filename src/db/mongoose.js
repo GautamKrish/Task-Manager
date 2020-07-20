97bc44a4874c360c407fb83189a2e287a7b48c12
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-07-20 10:04:16 +0530

    explored basic validation in mongoose and using validator npm library

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser : true,
@@ -8,43 +9,63 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 
 const User = mongoose.model('User',{
     name : {
-        type : String
+        type : String,
+        required : true,
+        trim : true
     },
     age : {
-        type : Number
-    }
-})
-
-// const me = new User({
-//     name : 'Gautam',
-//     age : 'Krishnan'
-// })
-
-// me.save()
-// .then(() =>{
-//     console.log( me )
-// })
-// .catch((error) => {
-//     console.log(`Error : - ${error}`)
-// })
-
-const Tasks = mongoose.model('Tasks', {
-    description :  {
-        type : String
+        type : Number,
+        default : 0,
+        validate(age) {
+            if(age < 0){
+                throw new Error('Age cannot be negative.')
+            }
+        }
     },
-    completed : {
-        type : Boolean
+    email : {
+        type : String,
+        required : true,
+        lowercase : true,
+        trim : true,
+        validate(email) {
+            if(!validator.isEmail(email)) {
+                throw new Error('Invalid email')
+            }
+        }
     }
 })
 
-const task = new Tasks({
-    description : 'node.js course',
-    completed : false
+const me = new User({
+    name : 'Shreyasi',
+    age : 23,
+    email : 'shreyasi@gmail.com'
 })
 
-task.save()
-.then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log(error)
+me.save()
+.then(() =>{
+    console.log( me )
 })
+.catch((error) => {
+    console.log(`Error : - ${error}`)
+})
+
+// const Tasks = mongoose.model('Tasks', {
+//     description :  {
+//         type : String
+//     },
+//     completed : {
+//         type : Boolean
+//     }
+// })
+
+// const task = new Tasks({
+//     description : 'node.js course',
+//     completed : false
+// })
+
+// task.save()
+// .then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log(error)
+// })
