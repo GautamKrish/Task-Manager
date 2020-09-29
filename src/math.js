5ecac33db622a64bfcad15dfcf997b318ca77210
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-09-29 19:41:52 +0530

    testing asynchronous code

--- src/math.js
+++ src/math.js
@@ -1,3 +1,18 @@
-const calculateTip = (total, percentTip = .25) => total + (total * percentTip)
+const calculateTip = (total, percentTip = 0.25) => total + total * percentTip;
+const fahrenheitToCelsius = (temp) => {
+  return (temp - 32) / 1.8;
+};
 
-module.exports = { calculateTip };
+const celsiusToFahrenheit = (temp) => {
+  return temp * 1.8 + 32;
+};
+
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (a < 0 || b < 0) reject("Numbers must be non negative");
+      resolve(a + b);
+    }, 2000);
+  });
+};
+module.exports = { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add };
--- tests/math.test.js
+++ tests/math.test.js
@@ -1,5 +1,5 @@
 const { TokenExpiredError } = require("jsonwebtoken");
-const { calculateTip } = require("../src/math");
+const { calculateTip, celsiusToFahrenheit, fahrenheitToCelsius, add } = require("../src/math");
 
 test("should calculate tip", () => {
     const total = calculateTip(10, .3);
@@ -11,8 +11,29 @@ test("should calculate tip on default percentage", () => {
     expect(total).toBe(12.5);
 });
 
+test('convert 0 C to  32 F', () => {
+    const fahrenheit = celsiusToFahrenheit(0);
+    expect(fahrenheit).toBe(32);
+});
+
+test('convert 32 F to 0 C', () => {
+    const celsius = fahrenheitToCelsius(32);
+    expect(celsius).toBe(0);
+})
 // test("Hello World", () => {});
 
 // test("This test should fail", () => {
 //   throw new Error("Failure");
 // });
+
+test('testing asynchronous add code', (done) => {
+    add(4, 5).then((sum) => {
+        expect(sum).toBe(9);
+        done();
+    })
+})
+
+test('testing asynchronous add code async/await', async () => {
+    const sum = await add(4, 5);
+    expect(sum).toBe(9);
+})
